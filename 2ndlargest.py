122b0e3851fef5a976d669db142911c5f9c52457
GuruPrakash <44807694+guru7520K@users.noreply.github.com>
2020-08-04 16:23:55 +0530

    Add files via upload

---
+++ 2ndlargest.py
@@ -0,0 +1,5 @@
+
+for _ in range(int(input())):
+    N=list(map(int,input().split()))
+    N.sort()
+    print(N[-2])
---
+++ SmallFact.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    n=int(input())
+    f=1
+    for i in range(1,n+1):
+        f=f*i
+    print(f)
